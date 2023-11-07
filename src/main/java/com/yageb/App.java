e172d664d46b24c55b473b60550623f628f527ee
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2023-11-07 17:56:36 +0100

    parse css

--- src/main/java/com/yageb/App.java
+++ src/main/java/com/yageb/App.java
@@ -1,6 +1,8 @@
 package com.yageb;
 
 import java.io.File;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 
@@ -14,20 +16,50 @@ import org.jsoup.select.*;
  * Hello world!
  */
 public final class App {
+    private static final String baseUrl = "http://books.toscrape.com/";
+
     private App() {
     }
 
     /**
      * Says hello to the world.
+     *
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
         Document doc;
 
         try {
-            final Response response = Jsoup.connect("http://books.toscrape.com/").execute();
+            final Response response = Jsoup.connect(baseUrl).execute();
             doc = response.parse();
 
+            Elements scripts = doc.getElementsByTag("script");
+            Elements css = doc.getElementsByTag("link");
+            for(Element c : css) {
+                String url = c.absUrl("href");
+                String rel = c.attr("rel") == null ? "" : c.attr("rel");
+                if(!url.isEmpty() && rel.equals("stylesheet")) {
+                    String filePath = url.split(baseUrl)[1];
+                    Document docScript = Jsoup
+                                            .connect(url) // add user agent
+                                            .ignoreContentType(true)
+                                            .get();
+
+                    String data = docScript.select("body").text();
+                    final File file = new File("data/" + filePath);
+                    FileUtils.writeStringToFile(file, data.toString());
+                }
+            }
+
+            // get js files
+
+            // get images
+            // Elements imgElements = doc.select("img[src]");
+            // for (Element imgElement : imgElements) {
+            //     String imgUrlString = imgElement.attr("abs:src");
+            //     URL imgURL = new URL(imgUrlString);
+            //     HttpURLConnection httpConnection = (HttpURLConnection) imgURL.openConnection();
+            // }
             final File file = new File("data/index.html");
             FileUtils.writeStringToFile(file, doc.outerHtml(), StandardCharsets.UTF_8);
         } catch (Exception e) {
