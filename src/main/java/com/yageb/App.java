464c3a66d5d61d5c4392f18d560adda65afa389a
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2023-11-07 15:58:59 +0100

    download html

--- src/main/java/com/yageb/App.java
+++ src/main/java/com/yageb/App.java
@@ -1,5 +1,15 @@
 package com.yageb;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+
+import org.apache.commons.io.FileUtils;
+import org.jsoup.*;
+import org.jsoup.Connection.Response;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+
 /**
  * Hello world!
  */
@@ -12,6 +22,16 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Document doc;
+
+        try {
+            final Response response = Jsoup.connect("http://books.toscrape.com/").execute();
+            doc = response.parse();
+
+            final File file = new File("data/index.html");
+            FileUtils.writeStringToFile(file, doc.outerHtml(), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
